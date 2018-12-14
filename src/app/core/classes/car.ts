afe3c92c622b0d8376d51d758555de4ef67cd064
Filipo Grand <filipogrand@gmail.com>
2018-12-13 21:19:26 -0500

    no message

--- src/app/core/classes/car.ts
+++
@@ -1,9 +0,0 @@
-export class Car {
-    id: number;
-    picture: string;
-    model: string;
-    year: number;
-    make: string;
-    price: string;
-    details: [];
-}
--- src/app/core/pipes/search-car.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchCarPipe } from './search-car.pipe';
-
-describe('SearchCarPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchCarPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
